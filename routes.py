9211e9f86dfb8d033b78da02a93beac2afc8cad3
Bence Fekete <fekete965@gmail.com>
2023-01-18 19:17:09 +0000

    Start Signup and Login implementation

--- routes.py
+++ routes.py
@@ -1,7 +1,8 @@
+from curses import flash
 from flask import abort, make_response, redirect, render_template, request, session
 from flask_login import login_required, login_user, logout_user
 
-from utils.main import isUrlSafe
+from utils.main import isUrlSafe, validatePassword
 from constants import ROUTES
 from app import App
 
@@ -17,25 +18,90 @@ def signUp():
 	if (request.method == "GET"):
 		return render_template("signup.html")
 
-	# Do form validation, and save user to the DB and redirect user to login page
+	# Gather form values
+	firstName = request.form.get("firstName")
+	lastName = request.form.get("lastName")
+	email = request.form.get("email")
+	password = request.form.get("password")
+	confirmation = request.form.get("confirmation")
+
+	# Check first name
+	if not firstName:
+		flash("First name is required")
+		return render_template("signup.html"), 400
+	# Check last name
+	if not lastName:
+		flash("Last name is required")
+		return render_template("signup.html"), 400
+	# Check email
+	if not email:
+		flash("Email is required")
+		return render_template("signup.html"), 400
+	# Check password
+	if not password:
+		flash("Password is required")
+		return render_template("signup.html"), 400
+	# Check password confirmation
+	if not confirmation:
+		flash("Please confirm your password")
+		return render_template("signup.html"), 400
+
+	# Validate the password
+	passwordErrorMsg = validatePassword(password, confirmation)
+	if passwordErrorMsg:
+		flash(passwordErrorMsg)
+		return render_template("signup.html"), 400
+
+	# Check if email is in use
+	# user = db.execute("SELECT * FROM users WHERE username = ? LIMIT 1;", username)
+
+	if len(user) != 0:
+		flash("Email is already in use")
+		return render_template("signup.html"), 400	
+
+	# Call userService to create a user
+ 	# userService.createUser(firstName, lastName, email, password)
 	return redirect(ROUTES.Login)
 
 
 # Define Login route
 @App.route(ROUTES.Login, methods=["GET", "POST"])
 def login():
-    if (request.method == "GET"):
-        return render_template("login.html")
+	if (request.method == "GET"):
+		return render_template("login.html")
 
-    next = request.args.get("next")
-    if not isUrlSafe(next):
-        return abort(400)
+	# Get form values
+	email = request.form.get("email")
+	password = request.form.get("password")
+ 
+	# Check email
+	if not email:
+		flash("Email required")
+		return render_template("login.html"), 400
+	# Check password
+	if not password:
+		flash("Password required")
+		return render_template("login.html"), 400
+ 
+	# Call userService to login the user
+ 	# user = userService.loginUser(email, password)
+
+	# Prompt the user 
+	if not user:
+		flash("Incorrect email or password")
+		return render_template("login.html"), 400
+
+	# Check if next url exists
+	next = request.args.get("next")
+	if not isUrlSafe(next):
+		return abort(400)
 
     # Get the user and save it
-    login_user("", remember=True)
+	login_user("", remember=True)
 
-    resp = make_response(redirect(next or ROUTES.Home))
-    return resp
+	# Redirect the user
+	resp = make_response(redirect(next or ROUTES.Home))
+	return resp
 
 
 # Define Logout route

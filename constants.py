30d6c7c71594e2417c1dc301e946dd3efd4f0371
Bence Fekete <fekete965@gmail.com>
2023-01-21 19:33:47 +0000

    Make custom enum

--- constants.py
+++ constants.py
@@ -3,12 +3,37 @@ from strenum import StrEnum
 RUSTY_CAULDRON_KEY = 'RUSTY_CAULDRON_KEY'
 MIN_PASSWORD_LENGTH = 8
 
-class ROUTES(StrEnum):
+class ExtendedEnum(StrEnum):
+    @classmethod
+    def list(cls):
+        return list(map(lambda c: c.value, cls))
+
+class ROUTES(ExtendedEnum):
     Home = "/"
     Signup = "/signup"
     Login = "/login"
     Logout = "/logout"
     Recipes= "/recipes"
     AddRecipe = "/recipes/new"
-    EditRecipe = "/recipes/<id>"
+    Recipe = "/recipes/<id>"
     MyRecipes = "/my-recipes"
+
+class COOKING_MEASUREMENT(ExtendedEnum):
+    cl = "centilitre"
+    cup = "cup"
+    dkg = "dekagram"
+    dl = "decilitre"
+    fl = "fluid-ounce"
+    g = "gramm"
+    gal = "gallon"
+    kg = "kilogram"
+    l = "litre"
+    lb = "pound"
+    mg = "miligram"
+    ml = "millilitre"
+    oz = "ounce"
+    pt = "pint"
+    qt = "quart"
+    t = "tonne"
+    tbs = "tablespoon"
+    tsp = "teaspoon"

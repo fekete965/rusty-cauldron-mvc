01a519a46a347170ff181b9e77c418eb98ba9a84
Bence Fekete <fekete965@gmail.com>
2023-03-23 22:00:29 +0000

    Update User class with UserMixins and override get_id for flask-session

--- tables/users.py
+++ tables/users.py
@@ -1,17 +1,19 @@
+from flask_login import UserMixin
 from app import db
+from utils.main import get_timestamp
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     _id = db.Column("id", db.Integer, primary_key=True)
     first_name = db.Column("first_name", db.String(100), nullable=False)
     last_name = db.Column("last_name", db.String(100), nullable=True)
-    email = db.Column("email", db.String(255), unique=True)
+    email = db.Column("email", db.String(255), unique=True, nullable=False)
     password = db.Column("password", db.String(255), nullable=False)
-    deleted = db.Column("deleted", db.Boolean, nullable=False)
-    created_at = db.Column("created_at", db.DateTime, nullable=False)
-    updated_at = db.Column("updated_at", db.DateTime, nullable=False)
+    deleted = db.Column("deleted", db.Boolean, nullable=False, default=False)
+    created_at = db.Column("created_at", db.DateTime, nullable=False, default=get_timestamp())
+    updated_at = db.Column("updated_at", db.DateTime, nullable=False, default=get_timestamp())
 
     def __init__(self, first_name, last_name, email, password, deleted, created_at, updated_at):
         self.first_name = first_name
@@ -21,3 +23,6 @@ class User(db.Model):
         self.deleted = deleted
         self.created_at = created_at
         self.updated_at = updated_at
+
+    def get_id(self):
+        return self._id

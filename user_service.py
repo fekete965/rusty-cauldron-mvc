691c2513d9e2bae7fd1a095cfe307d3060644775
Bence Fekete <fekete965@gmail.com>
2023-03-26 22:05:45 +0100

    Update the way we save new users

--- user_service.py
+++ user_service.py
@@ -1,10 +1,13 @@
-from sqlalchemy import insert
 from tables.users import User
-
+from app import db
 
 class UserService():
     def get_user_by_email(email):
         return User.query.filter(User.email == email).first()
 
-    def create_user(first_name, last_name, email, password, password_hash):
-        return insert(User).values(first_name=first_name, last_name=last_name, email=email, password=password, password_hash=password_hash)
+    def create_user(first_name, last_name, email, password):
+        newUser = User(first_name=first_name, last_name=last_name, email=email, password=password)
+        db.session.add(newUser)
+        db.session.commit()
+
+        return newUser

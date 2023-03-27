7c4969ba27166f9d830056a443c283fa2c64d930
Bence Fekete <fekete965@gmail.com>
2023-03-27 09:53:04 +0100

    Update styling in recipe_service

--- recipe_service.py
+++ recipe_service.py
@@ -9,55 +9,73 @@ class RecipeService():
 	def get_recipe_ids_from_filtered_ingredients(ingredients):
 		if len(ingredients) == 0:
 			return None
-		filteredIngredientFilter = [Ingredient.name.like(f"%{ingredient}%") for ingredient in ingredients]
-		return Ingredient.query.with_entities(Ingredient.recipe_id).filter(*filteredIngredientFilter).distinct().all()
+		filtered_ingredient_filter = [Ingredient.name.like(f"%{ingredient}%") for ingredient in ingredients]
+		return Ingredient.query.with_entities(Ingredient.recipe_id).filter(*filtered_ingredient_filter).distinct().all()
 
 
 	def get_recipes(user_id, title_opt, ingredients, page, per_page):
 		# Get the necessary recipe ids from the asked ingredient list
-		filteredIngredients = RecipeService.get_recipe_ids_from_filtered_ingredients(ingredients)
+		filtered_ingredients = RecipeService.get_recipe_ids_from_filtered_ingredients(ingredients)
 
 		# If we don't find any ingredients we can return an empty list
-		if filteredIngredients and len(filteredIngredients) == 0:
+		if filtered_ingredients and len(filtered_ingredients) == 0:
 			return list()
 
 		# Clean optional title
 		title = xstr(title_opt)
 		# Prepare recipe title filter
-		recipeFilter = Recipe.title.like(f"%{title}%")
+		title_filter = Recipe.title.like(f"%{title}%")
+		is_active_filter = Recipe.deleted == False
 		# Start recipe query
-		query = Recipe.query.filter(recipeFilter)
+		query = Recipe.\
+      		query.\
+			filter(title_filter, is_active_filter)
 		# Filter by user_id if needed
 		if (user_id):
 			query = query.filter(Recipe.user_id == user_id)
 		# If we have ingredient, add additional filter to the initial query
-		if filteredIngredients:
-			filteredIngredients = list(map(lambda i: i[0], filteredIngredients))
-			query = query.filter(Recipe._id.in_(filteredIngredients))
+		if filtered_ingredients:
+			filtered_ingredients = list(map(lambda i: i[0], filtered_ingredients))
+			query = query.filter(Recipe._id.in_(filtered_ingredients))
 
 		# Prepare order by
 		order_by = Recipe.created_at.desc()
 		# Add order by to query
 		query = query.order_by(order_by)
 		# Run final query with pagination
-		paginatedRecipes = query.paginate(page=page, per_page=per_page, error_out=False)
+		paginated_recipes = query.paginate(page=page, per_page=per_page, error_out=False)
 
-		recipeIdsOnPage = list(map(lambda r: r._id, paginatedRecipes.items))
-		ingredientsPerRecipe = {}
-		for recipeId in recipeIdsOnPage:
-			ingredients = Ingredient.query.filter(Ingredient.recipe_id == recipeId).order_by(Ingredient.name.desc()).all()
-			ingredientsPerRecipe[recipeId] = ingredients
+		recipe_ids_on_page = list(map(lambda r: r._id, paginated_recipes.items))
+		ingredients_per_recipe = {}
+		for recipeId in recipe_ids_on_page:
+			ingredients = Ingredient.\
+       			query.\
+              	filter(Ingredient.recipe_id == recipeId).\
+				order_by(Ingredient.name.desc()).\
+        		all()
+			ingredients_per_recipe[recipeId] = ingredients
 
-		return (paginatedRecipes, ingredientsPerRecipe)
+		return (paginated_recipes, ingredients_per_recipe)
 
 
 	def insert_recipe(user_id, form_data):
 		def toIngredient(recipe_id):
 			def fn(ingredient):
-				return Ingredient(recipe_id=recipe_id, name=ingredient["name"], amount=ingredient["amount"], measurement=ingredient["measurement"])
+				return Ingredient(
+        			recipe_id=recipe_id,
+           			name=ingredient["name"],
+              		amount=ingredient["amount"],
+                	measurement=ingredient["measurement"]
+				)
 			return fn
   
-		newRecipe = Recipe(user_id=user_id, title=form_data["title"], prep_time=form_data["prep_time"], cooking_time=form_data["cooking_time"], description=form_data["description"])
+		newRecipe = Recipe(
+      		user_id=user_id,
+        	title=form_data["title"],
+         	prep_time=form_data["prep_time"],
+          	cooking_time=form_data["cooking_time"],
+           	description=form_data["description"]
+		)
   
 		db.session.add(newRecipe)
 		db.session.commit()
@@ -66,7 +84,8 @@ class RecipeService():
 		db.session.add_all(ingredientList)
 		db.session.commit()
 
-		return None
+		return True
+
 
 	def mark_recipe_as_deleted(user_id, recipe_id):
 		db.session.query(Recipe).\
